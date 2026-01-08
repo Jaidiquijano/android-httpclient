b937be8a6e1719343cc92341ff542a7139118508
JaidiQuijano <jaidy33@hotmail.es>
2026-01-08 14:18:06 +0100

    se ha realizado varios puntos 4

--- app/src/main/java/es/fpsumma/dam2/api/viewmodel/TareasViewModel.kt
+++ app/src/main/java/es/fpsumma/dam2/api/viewmodel/TareasViewModel.kt
@@ -1,23 +1,36 @@
 package es.fpsumma.dam2.api.viewmodel
 
 import android.app.Application
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.Room
 import es.fpsumma.dam2.api.data.local.AppDatabase
 import es.fpsumma.dam2.api.data.local.entity.TareaEntity
+import es.fpsumma.dam2.api.model.Tarea
+import es.fpsumma.dam2.api.ui.screen.tareas.TareasUIState
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class TareasViewModel(app: Application) : AndroidViewModel(app) {
+
+
 
     private val db = Room.databaseBuilder(
         app, AppDatabase::class.java, "tareas.db"
     ).fallbackToDestructiveMigration(false).build()
 
     private val dao = db.tareaDao()
+
+    val state: StateFlow<TareasUIState> = dao.getAllTareas().map {
+        lista ->
+        TareasUIState(tareas =lista.map {
+            Tarea(it.id,it.titulo,it.descripcion) }
+        ) }
+        .stateIn(viewModelScope, SharingStarted.Lazily, TareasUIState())
 
     val tareas: StateFlow<List<TareaEntity>> =
         dao.getAllTareas().stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
